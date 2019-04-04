57367de4e0dc46db64e823a8fda39d02355d98f0
Edgar Valente <edvalente@gmail.com>
2019-04-04 17:21:19 -0300

    Adds helpers to calculate distances between point and center

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -17,3 +17,21 @@ def intersect_features(table1, table2, fraction):
     features_j = filter_relevant_features(table2, fraction)
     
     return list(set(features_i) & set(features_j))
+
+def calc_distance(a, b):
+    """
+    Calculates euclidean distance between two points
+    """
+    return np.sqrt((np.array(a) - np.array(b))**2).sum()
+
+def calc_distances(a, centers, indexes):
+    """
+    Calculates euclidean distances between one point and several
+    """
+    return {i: calc_distance(a[indexes], j[indexes]) for i, j in enumerate(centers)}
+
+def distances(individuals, centers, indexes):
+    """
+    Calculates euclidean distances between two arrays of points
+    """
+    return np.array([calc_distances(np.array(j), centers, indexes) for i, j in individuals.iterrows()])
