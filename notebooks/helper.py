bc0ecbadecc51f3f9d22da2cf20af475757cd950
Edgar Valente <edvalente@gmail.com>
2019-04-18 19:02:16 -0300

    Adds recommendation function

--- notebooks/helper.py
+++ notebooks/helper.py
@@ -1,7 +1,28 @@
 import pandas as pd
 import numpy as np
+from sklearn.metrics import f1_score, fbeta_score, precision_score, accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
+
+def custom_f1_score(y_pred, y_true):
+    labels = y_true.get_label()
+    y_bin = [1. if y_cont > 0.5 else 0. for y_cont in y_pred]
+    return 'f1_score', f1_score(labels, y_bin)
+
+def custom_fbeta_score(y_pred, y_true):
+    labels = y_true.get_label()
+    y_bin = [1. if y_cont > 0.5 else 0. for y_cont in y_pred]
+    return 'fbeta_score', fbeta_score(labels, y_bin, beta=0.5)
+
+def custom_precision_score(y_pred, y_true):
+    labels = y_true.get_label()
+    y_bin = [1. if y_cont > 0.5 else 0. for y_cont in y_pred]
+    return 'precision_score', precision_score(labels, y_bin)
+
+def custom_accuracy_score(y_pred, y_true):
+    labels = y_true.get_label()
+    y_bin = [1. if y_cont > 0.5 else 0. for y_cont in y_pred]
+    return 'accuracy_score', accuracy_score(labels, y_bin)
 
 def preprocess_minimal_data(df):
     """
@@ -60,3 +81,18 @@ def distances(individuals, centers, indexes):
     Calculates euclidean distances between two arrays of points
     """
     return np.array([calc_distances(np.array(j), centers, indexes) for i, j in individuals.iterrows()])
+
+def recommend(data, centers, rec_index, input_indexes, label_index):
+    
+    individuals_distances = distances(data, centers, input_indexes)
+    valid_clusters = [i for i, j in enumerate(centers) if j[label_index] > 0.75]
+    recommended_values = {k: v[rec_index] for k, v in enumerate(centers) if k in valid_clusters}
+    
+    recommendations = []
+    for individual_distance in individuals_distances:
+        valid_distances = {k: v for k, v in individual_distance.items() if k in valid_clusters}
+        recommended_cluster = min(valid_distances, key=valid_distances.get)
+        recommended_value = int(recommended_values[recommended_cluster])
+        recommendations.append(recommended_value)
+        
+    return np.array(recommendations)
